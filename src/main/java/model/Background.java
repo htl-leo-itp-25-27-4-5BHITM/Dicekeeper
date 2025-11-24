63b9e7267eb09a2753df3c7684000f5f9add465b
dmahringer <daniel.mahringer@gmail.com>
2025-11-24 11:40:13 +0100

    #20 - updated model class to fit tanja

--- src/main/java/model/Background.java
+++ src/main/java/model/Background.java
@@ -2,12 +2,14 @@ package model;
 
 import io.quarkus.hibernate.orm.panache.PanacheEntity;
 import jakarta.persistence.Entity;
+import jakarta.persistence.JoinColumn;
 
 @Entity
 public class Background extends PanacheEntity {
     public String name;
     public String description;
-//    public String skill_proficiencies;
-//    public String tool_proficiencies;
-//    public String languages;
+    public String skill_proficiencies;
+    public String equipment;
+    public String languages;
+    public String feat;
 }
