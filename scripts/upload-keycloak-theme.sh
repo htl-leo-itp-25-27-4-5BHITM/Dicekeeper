70b596a9981aff46d183395d1d5c8d79898b8c71
Niklas Satzinger <niklas@blauregen.dev>
2026-03-25 07:35:34 +0100

    Automatical email template update

--- scripts/upload-keycloak-theme.sh
+++ scripts/upload-keycloak-theme.sh
@@ -38,4 +38,4 @@ while IFS= read -r -d '' file; do
 done < <(find "$THEME_DIR" -type f -print0)
 
 echo "Theme uploaded to $TARGET_DIR"
-echo "Now restart the Keycloak pod and select Email Theme = ${THEME_NAME}"
+echo "Now select the Email Theme = ${THEME_NAME}"
